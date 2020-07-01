6194cc68715764af150a2370a0786411f82f28e0
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-07-01 16:16:22 -0500

    Update to title generator
    An initial commit

---
+++ Title_Generator/index.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button");
+const spanA = document.getElementById("A");
+const spanB= document.getElementById("B");
+
+//======================================================================================================================
+const noun = ["Sales", "Marketing", "HTML", "JavaScript", "Accounting", "IT", "Recruitng", "Human Resources", "People", "Software", "Charity", "Underling", "Brand", "Graphic", "Social Media", "Media Relations", "eCommerce", "Industry", "Chief", "Unemployed", "Digital", "Project", "Code", "Python", "Semantic", "Meaningful", "C#", "Swift", "Agile", "Business"];
+const title = ["Engineer", "Specialist", "Associate", "Analyst", "Advocate", "Student", "Champion", "Founder", "Executive", "Manager", "Agent", "Coder", "Programmer", "Hacker", "Priest", "High Priest", "Shaman", "Sales Manager", "Account Executive", "Mancer", "Head", "Lead", "Moron", "Developer", "Buyer", "Assistant", "Copywriter", "Coordinator", "Hero", "Programmer", "Journalist", "Line Cook", "Cashier", "Player", "Chief", "Warrior", "Lich", "Ninja", "Rockstar", "Paladin", "Scaler", "Builder", "Priest", "Rogue", "Receptionist", "Officer", "At Large", "At Law", "PhD", "Thief", "-Obsessed", "Juggler", "Wizard", "Sensei", "Extremist", "Zealot"];
+//======================================================================================================================
+
+button.addEventListener("click", function(){
+    const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+    const randomTitle = title[Math.floor(Math.random() * title.length)];
+    spanA.classList.remove(".hidden");
+    
+    //Update the spans
+    spanA.textContent = randomNoun;
+    spanB.textContent = randomTitle;
+    console.log("Your new title is... " + randomNoun +" "+ randomTitle);
+});
